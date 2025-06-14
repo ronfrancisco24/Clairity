02e51c09dd854a74ce9264bb236af5b70068f57a
ronfrancisco24 <aaronmatt2004@gmail.com>
2025-06-14 13:30:56 +0800

    created dashboard screen file

---
+++ mobile-app/lib/screens/dashboard_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
