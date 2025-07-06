e558924320cc727d6006c1f35c8737a5e5158ce7
ronfrancisco24 <aaronmatt2004@gmail.com>
2025-07-07 07:44:14 +0800

    update: changes to sign in screen

--- mobile-app/lib/screens/sign_in_screen.dart
+++ mobile-app/lib/screens/sign_in_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../widgets/bottomsheet.dart';
 import '../widgets/auth_text_field.dart';
+import '../widgets/sign_button.dart';
+import '../widgets/social_button.dart';
 import 'sign_up_screen.dart';
 
 class SignInScreen extends StatefulWidget {
@@ -14,48 +16,12 @@ class _SignInScreenState extends State<SignInScreen> {
   bool? isChecked = false;
 
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    // TODO: do something here.
-    // call show modal bottom sheet.
-
-    // to allow the state to be initialized first before show bottom sheet
-    Future.delayed(Duration.zero, () {
-      showModalBottomSheet(
-        context: context,
-        isScrollControlled: true,
-        isDismissible: false,
-        enableDrag: false,
-        builder: (context) => Bottomsheet(
-          formFields: [
-            AuthTextField(title: 'Full Name'),
-            AuthTextField(title: 'Email'),
-            AuthTextField(title: 'Password'),
-            AuthTextField(title: 'Confirm Password'),
-          ],
-          onSignUp: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => SignUpScreen(),
-              ),
-            );
-          },
-          isChecked: isChecked,
-          showCheckbox: true,
-        ),
-      );
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.bottomLeft,
             end: Alignment.topRight,
@@ -63,6 +29,98 @@ class _SignInScreenState extends State<SignInScreen> {
               Color(0xFF0B334D),
               Color(0xFF477023),
             ],
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(25),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Center(
+                  child: Text(
+                    'Clairity',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Sign Up',
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Just some details to get you in!',
+                        style: TextStyle(fontSize: 15, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+                const AuthTextField(title: 'Email/Phone'),
+                const AuthTextField(title: 'Password'),
+                const AuthTextField(title: 'Confirm Password'),
+                SignButton(
+                  onTap: () {},
+                ),
+                Container(
+                  child: const Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          color: Color(0xFF37964E),
+                          thickness: 2,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Text(
+                          'Or',
+                          style: TextStyle(
+                            color: Color(0xFF37964E),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          color: Color(0xFF37964E),
+                          thickness: 2,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SocialButton(
+                  onTap: () {},
+                ),
+                const Center(
+                  child: Text(
+                    'Already Registered? Login Here.',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text('Terms & Conditions'),
+                    Text('Support'),
+                    Text('Customer Care')
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                )
+              ],
+            ),
           ),
         ),
       ),
--- mobile-app/lib/widgets/sign_button.dart
+++ mobile-app/lib/widgets/sign_button.dart
@@ -9,32 +9,29 @@ class SignButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 50),
-        child: Container(
-          width: double.infinity,
-          height: 50,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color(0xFF0B334D),
-                Color(0xFF477023),
-              ],
-            ),
-            borderRadius: BorderRadius.circular(15),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.2), // shadow color
-                blurRadius: 10, // how blurry the shadow is
-                offset: Offset(0, 4), // horizontal and vertical shift
-              ),
+      child: Container(
+        width: double.infinity,
+        height: 50,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF0B334D),
+              Color(0xFF477023),
             ],
           ),
-          child: Center(
-            child: Text(
-              'Sign Up',
-              style: TextStyle(color: Colors.white),
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2), // shadow color
+              blurRadius: 10, // how blurry the shadow is
+              offset: Offset(0, 4), // horizontal and vertical shift
             ),
+          ],
+        ),
+        child: Center(
+          child: Text(
+            'Sign Up',
+            style: TextStyle(color: Colors.white),
           ),
         ),
       ),
