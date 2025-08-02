790ace61218385ecec7d78603d3964a8df588b42
ronfrancisco24 <aaronmatt2004@gmail.com>
2025-08-02 14:04:56 +0800

    added firebase

--- mobile-app/lib/main.dart
+++ mobile-app/lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'firebase_options.dart';
 import 'screens/onboarding/splash_screen.dart';
 import 'package:provider/provider.dart';
 import 'providers/log_provider.dart';
 import 'utils/navbar_utils.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     MultiProvider(
       providers: [
