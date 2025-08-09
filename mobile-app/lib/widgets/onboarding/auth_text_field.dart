fda98c6f561b4209a62d040f87a02a2ed1670597
Adrian Kyle Mariano <167556034+AKSM-TFT@users.noreply.github.com>
2025-08-10 00:57:46 +0800

    Update: OTP Screen
    -  Added fixed country code (+63), both in UI and Logic

--- mobile-app/lib/widgets/onboarding/auth_text_field.dart
+++ mobile-app/lib/widgets/onboarding/auth_text_field.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AuthTextField extends StatefulWidget {
-  final String title;
+  final String hint;
   final bool showToggle;
   final bool obscureText;
   final TextEditingController controller;
+  final String type;
 
-  const AuthTextField(
-      {super.key,
-      required this.title,
-      required this.controller,
-      this.showToggle = false,
-      this.obscureText = false});
+  const AuthTextField({
+    super.key,
+    required this.hint,
+    required this.controller,
+    required this.type,
+    this.showToggle = false,
+    this.obscureText = false,
+  });
 
   @override
   State<AuthTextField> createState() => _AuthTextFieldState();
@@ -28,42 +32,51 @@ class _AuthTextFieldState extends State<AuthTextField> {
 
   @override
   Widget build(BuildContext context) {
+    final String countryCode = "+63";
+
     return Container(
       decoration: BoxDecoration(
         color: Colors.transparent,
         borderRadius: BorderRadius.circular(10),
         border: Border.all(width: 2, color: Colors.white),
       ),
-      child: Padding(
-        padding: EdgeInsets.all(5.0),
-        child: TextField(
-          controller: widget.controller,
-          obscureText: _obscure,
-          style: TextStyle(color: Colors.white),
-          textAlignVertical: TextAlignVertical.center,
-          decoration: InputDecoration(
-            labelStyle: TextStyle(color: Colors.white),
-            labelText: widget.title,
-            suffixIcon: widget.showToggle
-                ? IconButton(
-                    onPressed: () {
-                      setState(() {
-                        _obscure = !_obscure;
-                      });
-                    },
-                    icon: Icon(
-                      _obscure
-                          ? Icons.remove_red_eye_outlined
-                          : Icons.remove_red_eye,
-                      color: Colors.white,
-                    ),
-                  )
-                : null,
-            border: InputBorder.none,
-            floatingLabelBehavior: FloatingLabelBehavior.never,
-            contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+      child: TextField(
+        controller: widget.controller,
+        obscureText: _obscure,
+        style: const TextStyle(color: Colors.white),
+        textAlignVertical: TextAlignVertical.center,
+        decoration: InputDecoration(
+          prefix: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.w),
+            child: Text(
+              widget.type == "phone" ? countryCode : "",
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
+          hintText: widget.hint,
+          hintStyle: const TextStyle(color: Colors.white54),
+          suffixIcon: widget.showToggle
+              ? IconButton(
+            onPressed: () {
+              setState(() {
+                _obscure = !_obscure;
+              });
+            },
+            icon: Icon(
+              _obscure
+                  ? Icons.remove_red_eye_outlined
+                  : Icons.remove_red_eye,
+              color: Colors.white,
+            ),
+          )
+              : null,
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.symmetric(vertical: 16),
         ),
+        keyboardType: TextInputType.phone,
       ),
     );
   }
