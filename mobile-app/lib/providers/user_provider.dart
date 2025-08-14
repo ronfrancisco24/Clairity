c6f0a6bb70c712db769a710e84fe16bad86ca271
Adrian Kyle Mariano <167556034+AKSM-TFT@users.noreply.github.com>
2025-08-14 16:58:56 +0800

    Add: Add change avatar feature
    - Modify user model for avatar index
    - fixed avatar selections
    - update avatar
    - get avatar and display in records
    - show username of cleaner  in database

--- mobile-app/lib/providers/user_provider.dart
+++ mobile-app/lib/providers/user_provider.dart
@@ -1,50 +1,35 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import '../models/user_model.dart';
 import '../services/user_datebase_service.dart';
-
-final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+import '../services/profile_service.dart';
 
 class UserProvider extends ChangeNotifier {
   // User details-related
+  final ProfileService _profileService = ProfileService();
   UserModel? _user;
   UserModel? get user => _user;
   bool get isLoaded => _user != null;
 
-  Future<UserModel?> getUserDetails() async {
-    final user = FirebaseAuth.instance.currentUser;
-    if (user != null) {
-      final uid = user.uid;
-      DocumentSnapshot doc = await _firestore.collection('users').doc(uid).get();
-
-      if (doc.exists) {
-        return UserModel.fromMap(uid, doc.data() as Map<String, dynamic>);
-      } else {
-        print('User document not found in Firestore.');
-      }
-    }
-    return null;
-  }
-
-  Stream<UserModel?> getUserDetailsStreamById(String uid) {
-    return _firestore.collection('users').doc(uid).snapshots().map((doc) {
-      if (doc.exists) {
-        return UserModel.fromMap(uid, doc.data() as Map<String, dynamic>);
-      } else {
-        return null;
-      }
-    });
-  }
-
   Future<void> loadUserData() async {
-    _user = await getUserDetails();
+    _user = await _profileService.getCurrentUserProfile();
+    notifyListeners();
+  }
+
+  Stream<UserModel?> get userStream => _profileService.getCurrentUserProfileStream();
+
+  Future<void> changeAvatar(int avatarIndex) async {
+    await _profileService.updateUserAvatar(avatarIndex);
+    _user = await _profileService.getCurrentUserProfile();
     notifyListeners();
   }
 
   void clearUserData() {
     _user = null;
     notifyListeners();
+  }
+
+  Stream<UserModel?> userStreamById(String uid) {
+    return _profileService.getUserDetailsStreamById(uid);
   }
 
   // User management database
